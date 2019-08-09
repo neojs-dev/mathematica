8f6667b658338d45c83253b2db14bad4c84cba0b
Will Hoskings <resynth1943@outlook.com>
2019-08-09 13:47:53 +0100

    Add type-checking for operands

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const isNumber = require('is-number');
+
 module.exports = function math(x, operator, y) {
+  if (!isNumber(x)) {
+    throw new Error('Expected the first operand to be a number.');
+  }
+
+  if (!isNumber(y)) {
+    throw new Error('Expected the second operand to be a number.');
+  }
+
   return Number(operator(x, y))
 }
